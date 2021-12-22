7e1a77b3829e98c14397a125b1e4e039dcf6e8cf
Lamara2000 <mouzni.lamara@gmail.com>
2021-12-22 17:04:27 +0100

    Analyse syntaxique

--- src/main/java/tla/Test.java
+++ src/main/java/tla/Test.java
@@ -16,8 +16,25 @@ public class Test {
 		}
 	}
 	
+	public static void testParser(String entree) {
+		try {
+			System.out.println("début d'analyse lexicale");
+			List<Token> tokens = AnalyseLexicale.analyse(entree);
+			for (Token t : tokens) {
+				System.out.println(t);
+			}
+			System.out.println("fin d'analyse lexicale");
+			System.out.println("début d'analyse syntaxique");
+			Node root = new AnalyseSyntaxique().analyse(tokens);
+			System.out.println("fin d'analyse syntaxique");
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+	}
+	
 	public static void main(String[] args) {
-		testLexer("labyrinthe(12,12) mur(1,0) joueur(0,0)");
+		//testLexer("labyrinthe(12,12) mur(1,0) joueur(0,0)");
+		testParser("labyrinthe(12,12) mur(1,0)(4,5)(7,9) joueur(0,0) sortie(7,8) fantome(1,2)(4,7)(2,8) masse 4 (0,6) bouclier 5 (1,8)");
 	}
 
 }
